6151c86dbb584cdb1b600feb27504e2817d5f62b
mattmcgiv <matt@antym.com>
2015-02-11 08:31:53 -0500

    Removes Home | ... from home page <title>.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,23 +1,23 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-  page_title_suffix = "| Ruby on Rails Tutorial Sample App"
+  page_title_suffix = "Ruby on Rails Tutorial Sample App"
 
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home #{page_title_suffix}"
+    assert_select "title", "#{page_title_suffix}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help #{page_title_suffix}"
+    assert_select "title", "Help | #{page_title_suffix}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About #{page_title_suffix}"
+    assert_select "title", "About | #{page_title_suffix}"
   end
 end
